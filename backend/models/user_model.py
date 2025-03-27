84fd6cc85c39619d5eb833c7e79541d3918d032d
Mayur Behere <mayurbehere7432@gmail.com>
2025-03-27 15:21:42 +0530

    restart

--- backend/models/user_model.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, uid, name, email):
-        self.uid = uid
-        self.name = name
-        self.email = email
-
-    def to_dict(self):
-        return {"uid": self.uid, "name": self.name, "email": self.email}
